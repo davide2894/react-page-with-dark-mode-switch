ac615c57168d5b28c70d15c5fb23de8f2cc9a09a
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-06-14 19:30:11 +0200

    initial setup

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import "./Navbar.scss";
+
+export default function Navbar() {
+  return (
+    <nav 
+      className={props.darkMode ? "dark": ""}
+    >
+      <img 
+          className="nav--logo_icon"
+          src="./images/react-icon-small.png"
+      />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      
+      <div 
+          className="toggler" 
+      >
+          <p className="toggler--light">Light</p>
+          <div 
+              className="toggler--slider"
+              onClick={props.toggleDarkMode}
+          >
+              <div className="toggler--slider--circle"></div>
+          </div>
+          <p className="toggler--dark">Dark</p>
+      </div>
+    </nav>
+  )
+}
