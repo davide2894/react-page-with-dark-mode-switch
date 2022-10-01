497af2e9281ed662bb7aa382b2161b3ffa86f3ab
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-10-01 10:17:02 +0200

    style: improve overall style

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,14 +5,16 @@ export default function Main(props) {
 
   return (
     <main className={classNames}>
-        <h1 className="main__title">Fun facts about React</h1>
-        <ul className="main__facts facts">
-            <li className="facts__li">Was first released in 2013</li>
-            <li className="facts__li">Was originally created by Jordan Walke</li>
-            <li className="facts__li">Has well over 100K stars on GitHub</li>
-            <li className="facts__li">Is maintained by Facebook</li>
-            <li className="facts__li">Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+        <div className="main__textContainer">
+          <h1 className="main__title">Fun facts about React</h1>
+          <ul className="main__facts facts">
+              <li className="facts__li">Was first released in 2013</li>
+              <li className="facts__li">Was originally created by Jordan Walke</li>
+              <li className="facts__li">Has well over 100K stars on GitHub</li>
+              <li className="facts__li">Is maintained by Facebook</li>
+              <li className="facts__li">Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+        </div>
     </main>
   )
 }
